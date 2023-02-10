c6ea3c366d384dadaf371bd4037b9adf2404ee24
Gustavo <gustavoalvdev@hotmail.com>
2023-02-10 06:29:19 -0500

    Implement exercise 15

---
+++ ejercicios/ejercicio_13.py
@@ -0,0 +1,7 @@
+#Crear un programa que pida al usuario ingresar dos números y calcule su suma, resta, multiplicación y división
+
+suma = 1 + 5 + 10
+resta = 1 - 5 - 10
+multiplicacion = 1 * 5 * 10
+division = 1 / 5 / 10
+print("Suma: " , suma ,  "\nResta: " , resta , "\nMultiplicación: " , multiplicacion , "\nDivisión: " , division)
--- ejercicios/ejercicio_15.py
+++ ejercicios/ejercicio_15.py
@@ -1,1 +1,6 @@
 #
+palabra = input()
+if str(palabra) == str(palabra)[::-1] :
+    print("Palindromo")
+else:
+    print("Not Palindromo")
